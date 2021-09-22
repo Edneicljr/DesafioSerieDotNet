5ff3306dadd7e41cb0443c9749e3743ec95e55bf
Your Name <you@example.com>
2021-09-22 12:19:44 -0300

    Commit Curso

---
+++ Classes/Curso.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using DesafioAppCadastroDotNet;
+
+namespace DesafioAppCadastroDotNet
+{
+    public class Curso : EntidadeBase
+     {
+         
+        // Atributos
+		private Tema Tema { get; set; }
+		private string Titulo { get; set; }
+		private string Descricao { get; set; }
+		private string EnderecoWeb { get; set; }
+		private int Duracao { get; set; }
+		private bool Excluido {get; set;}
+        
+
+        // Métodos
+		public Curso(int id, Tema tema, string titulo, string descricao, string enderecoWeb, int duracao)
+		{
+          	this.Id = id;
+			this.Tema = tema;
+			this.Titulo = titulo;
+			this.Descricao = descricao;
+			this.EnderecoWeb = enderecoWeb;
+			this.Duracao = duracao;
+            this.Excluido = false;
+		}
+
+        public Curso(int id, Tema tema, string titulo, int duracao, string descricao)
+        {
+            Id = id;
+            Tema = tema;
+            Titulo = titulo;
+            Duracao = duracao;
+            Descricao = descricao;
+        }
+
+        public override string ToString()
+		{
+			// Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + this.Tema + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+			retorno += "Descrição: " + this.EnderecoWeb + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Duracao + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+			return retorno;
+		}
+
+        public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
+
+		public int retornaId()
+		{
+			return this.Id;
+		}
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir() {
+            this.Excluido = true;
+        }
+    
+        
+    }
+}
